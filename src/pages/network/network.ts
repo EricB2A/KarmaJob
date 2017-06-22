9c0f54e0e188db9d9350e8c86ecba0d54b196420
Eric <eric.bousbaa@hotmail.com>
2017-06-22 10:28:30 +0200

    Auto stash before revert of "Configuration updates"

---
+++ src/pages/network/network.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import {AlertController, IonicPage, NavController, Platform} from 'ionic-angular';
+// import {Network} from "ionic-native";
+import {Network} from 'ionic-native';
+
+declare var navigator: any;
+declare var Connection: any;
+
+@IonicPage()
+@Component({
+  selector: 'page-network',
+  templateUrl: 'network.html',
+})
+export class NetworkPage {
+
+  constructor(private navCtrl: NavController, private platform: Platform, private alertCtrl: AlertController) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad NetworkPage');
+  }
+
+  checkNetwork() {
+    this.platform.ready().then(() => {
+      // console.log(navigator.connection.ty);
+      // console.log(Network);
+      var networkState = navigator.connection.type;
+      var states = {};
+      states[Connection.UNKNOWN]  = 'Unknown connection';
+      states[Connection.ETHERNET] = 'Ethernet connection';
+      states[Connection.WIFI]     = 'WiFi connection';
+      states[Connection.CELL_2G]  = 'Cell 2G connection';
+      states[Connection.CELL_3G]  = 'Cell 3G connection';
+      states[Connection.CELL_4G]  = 'Cell 4G connection';
+      states[Connection.CELL]     = 'Cell generic connection';
+      states[Connection.NONE]     = 'No network connection';
+      let alert = this.alertCtrl.create({
+        title: "Connection Status",
+        subTitle: states[networkState],
+        buttons: ["OK"]
+      });
+      alert.present();
+    });
+  }
+
+}
